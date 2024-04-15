3815a9e82fbaaf18a21514b41d659c4f36fdb0e6
Patajavi <167017845+Patajavi@users.noreply.github.com>
2024-04-14 17:38:22 -0700

    Create Diccionarios1.py

---
+++ Diccionarios1.py
@@ -0,0 +1,13 @@
+Calificaciones={
+"Jose": 8,
+"Angel": 10,
+"Fernando": 9,
+"Tonatiuh": 8,
+"Javier": 9,
+"Jorge": 9,
+"Diego": 7,
+"Sergio": 8,
+"Jesus": 6,
+"Ramon": 7
+}
+print("La calificacion es ",Calificaciones["Javier"])
