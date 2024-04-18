2f0e0a6a4ff612aeaba07a01dc86bd7554330b90
Patajavi <167017845+Patajavi@users.noreply.github.com>
2024-04-18 14:40:22 -0700

    Update diccionarios_t5.py

--- diccionarios_t5.py
+++ diccionarios_t5.py
@@ -184,3 +184,22 @@ if esta_vacio:
     print("El diccionario está vacío.")
 else:
     print("El diccionario no está vacío.")
+
+#9 Escribe un programa en Python para extraer una lista de valores de una lista dada de diccionarios. Diccionario original:[{'Matematicas': 90, 'ciencia': 92}, {'matematicas': 89, 'ciencia': 94}, {'Matematicas': 92, 'ciencia': 88}]
+    
+#10 Extrae una lista de valores de dicha lista de diccionarios donde la asignatura = Ciencias [92, 94, 88] Diccionario original: [{'matematicas': 90, 'ciencia': 92}, {'Matematicas': 89, 'ciencia': 94}, {'Matematicas': 92, 'ciencia': 88}] 
+
+#11 Extrae una lista de valores de dicha lista de diccionarios donde la asignatura = Matemáticas [90, 89, 92]
+
+#12 Escribe un programa en Python para encontrar la longitud de un diccionario de valores.
+
+#13 Escribe un programa en Python para obtener la profundidad de un diccionario.
+
+#14 Escribe un programa en Python para acceder al elemento de la clave de un diccionario por índice. Salida esperada: física matemáticas química
+
+#15 Escribe un programa en Python para convertir un diccionario en una lista de listas.
+#Diccionario original: {1: 'rojo', 2: 'verde', 3: 'negro', 4: 'blanco', 5: 'negro'} Convierte el mencionado diccionario en una lista de listas: [[1, 'rojo'], [2, 'verde'], [3, 'negro'], [4, 'blanco'], [5, 'negro']]
+
+#16 Escribe un programa en Python para filtrar números pares de un diccionario de valores.
+#Diccionario original: {'V': [1, 4, 6, 10], 'VI': [1, 4, 12], 'VII': [1, 3, 8]} Filtra números pares de los valores del diccionario mencionado: {'V': [4, 6, 10], 'VI': [4, 12], 'VII': [8]}
+#Diccionario original: {'V': [1, 3, 5], 'VI': [1, 5], 'VII': [2, 7, 9]} Filtra números pares de los valores del diccionario mencionado: {'V': [], 'VI': [], 'VII': [2]}
