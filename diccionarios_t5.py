503f7cec0f1105c24cb5812d095b0ab6a18cb00d
Patajavi <167017845+Patajavi@users.noreply.github.com>
2024-04-18 13:36:24 -0700

    Update diccionarios_t5.py

--- diccionarios_t5.py
+++ diccionarios_t5.py
@@ -1,19 +1,22 @@
 #1 Escribe un programa en Python para multiplicar todos los elementos de un diccionario.
 def multiplicar_valores_diccionario(diccionario):
-    producto = 1  #inicializar el producto en 1
-    for i in diccionario.values():
-        #verificar si el valor es un número (int o float)
-        if isinstance(i, (int, float)):
-            #multiplicar el producto con el valor actual
-            producto*=i
+    producto=1#inicializar el producto en 1
+    for i,j in diccionario.items():
+        if isinstance(j, dict):
+            #si el valor es otro diccionario, hacer una llamada recursiva a sí misma
+            producto*=multiplicar_valores_diccionario(j)
+        elif isinstance(j, (int, float)):
+            #si el valor es un número (int o float), multiplicarlo con producto
+            producto*=j
         else:
-            print(f"El valor '{i}' no es un número, por lo que será ignorado.")
+            #ignorar los valores que no sean numéricos
+            print(f"El valor asociado a '{i}' no es numérico y será ignorado.")
     
     return producto
 
 diccionario={
     "a":2,
-    "b":3,
+    "b":{"b_1":3,"b_2":"nonumero"},
     "c":4
 }
     
